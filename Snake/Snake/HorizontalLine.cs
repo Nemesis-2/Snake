2302b255eedc41d31e8c83f1f522f45596fe6aec
Nemesis <prokek@bk.ru>
2019-01-29 21:50:27 +0300

    Класс Горизонтальная линия

---
+++ Snake/Snake/HorizontalLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class HorizontalLine
+    {
+        List<Point> qList;
+
+        public HorizontalLine(int xLeft,int yRhigt, int y, char sym)
+        {
+            qList = new List<Point>();
+            for(int x = xLeft; x <= yRhigt; x++)
+            {
+                Point q = new Point(x, y, sym);
+                qList.Add(q);
+            }
+        }
+
+        public void Drow()
+        {
+            foreach (Point q in qList)
+            {
+                q.Draw();
+            }
+        }
+    }
+}
