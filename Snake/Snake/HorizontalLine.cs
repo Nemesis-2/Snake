13a6ee5acd8cddb3b659acffacdf60a024fd5cd3
Nemesis <prokek@bk.ru>
2019-01-29 22:18:00 +0300

    Вертикальная линия и Фигуры

---
+++ Snake/Snake/HorizontalLine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class HorizontalLine : Figure
+    {
+
+        public HorizontalLine(int xLeft,int xRhigt, int y, char sym)
+        {
+            qList = new List<Point>();
+            for(int x = xLeft; x <= xRhigt; x++)
+            {
+                Point q = new Point(x, y, sym);
+                qList.Add(q);
+            }
+        }
+
+    }
+}
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,14 +10,21 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+
+            HorizontalLine Upline = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine Downline = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine Rightline = new VerticalLine(0, 24, 0, '-');
+            VerticalLine Leftline = new VerticalLine(0, 24, 78, '-');
+            Rightline.Drow();
+            Leftline.Drow();
+            Upline.Drow();
+            Downline.Drow();
+
             Point q1 = new Point(1, 3, '*');
             q1.Draw();
 
             Point q2 = new Point(4, 5, '#');
             q2.Draw();
-
-            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
-            line.Drow();
 
             Console.ReadLine();
         }
